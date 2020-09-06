7f0f91163dba51658d6f64f2f6521ee202006f3f
Pedro Lopes <pedrolopes@Pedros-MacBook-Pro.local>
2020-09-06 11:33:08 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"; // removed , { Component } because it was not being used 
+import Routes from "./routes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './styles.css'
+
+import Header from './components/Header';
+// import Main from "./pages/main"; removed because <Main /> was replaced by <Routes />
+
+const App = () => (
+  <div className="App">
+      <Header />
+      <Routes />
     </div>
-  );
-}
+);
 
 export default App;
